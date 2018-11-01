fc2f9f67c1b456423da90393aa2a10dee988d642
Logan <logan.christopher@comcast.net>
2018-11-01 00:01:29 -0500

    - comments added

--- CSElab8/baby_names.py
+++ CSElab8/baby_names.py
@@ -7,6 +7,7 @@
 # Program Description: Program opens a list of popular baby names and ranks a user inputed name against the US most popular names.
 
 def main():
+  # searches through files and checks for match between user input name and list name
   def find_name(name, gender, file_name): 
     open_file = list(open(file_name, 'r'))
   
@@ -36,6 +37,7 @@ def main():
           if name.capitalize() == line.split()[3]:
             return line.split()[0], year
       
+  # Checks whther name entered is a double name and if so, executes seperate functions in order to get results for both names
   def dub_name(first, last, file_name):
     if type(file_name) is list:
       for fn in file_name:
@@ -69,6 +71,7 @@ def main():
         except:
           print('No Results Found')
 
+  # splits entered name if double name and later on sends first and last names to dub_check
   def split_name(baby_name):
     if ' ' in baby_name:
       first, last = baby_name.split()
@@ -78,6 +81,7 @@ def main():
       last = None
       return first, last
 
+  # validates gender entered
   def val_gender(gender):
     while True:
       if gender == 'm' or gender == 'f':
@@ -86,6 +90,7 @@ def main():
         print('Please enter valid gender.')
         gender = input('Enter the gender of the baby (m/f):')
 
+  # validates file entered
   def val_file():
     while True:
       file_name = input('Please enter the file name (with extension):')
@@ -100,8 +105,10 @@ def main():
       except FileNotFoundError:
         print('File Not Found. Try Again.')
 
+  # starts program by validating file entered by user
   val, file_name = val_file()
 
+  # if file is validated, continue with program
   if val:
     baby_name = input('Enter baby name:')
     gender = input('Enter the gender of the baby (m/f):')
@@ -110,5 +117,5 @@ def main():
     first, last = split_name(baby_name)
     dub_name(first, last, file_name)      
     
-
+# executes main function
 main()
